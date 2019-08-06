a0913078b4ac7b9dd63847135a4a512cb8960363
Mian Qin <celery1124@tamu.edu>
2019-08-06 16:58:11 -0500

    more comments on kvrangedb Read/Write options

--- include/kvrangedb/options.h
+++ include/kvrangedb/options.h
@@ -98,6 +98,8 @@ struct Options {
 
 // Options that control read operations
 struct ReadOptions {
+  // Define the upper key (Non-Inclusive) for range query
+  // Default: NULL
   const Slice* upper_key;
 
   ReadOptions()
@@ -107,21 +109,7 @@ struct ReadOptions {
 
 // Options that control write operations
 struct WriteOptions {
-  // If true, the write will be flushed from the operating system
-  // buffer cache (by calling WritableFile::Sync()) before the write
-  // is considered complete.  If this flag is true, writes will be
-  // slower.
-  //
-  // If this flag is false, and the machine crashes, some recent
-  // writes may be lost.  Note that if it is just the process that
-  // crashes (i.e., the machine does not reboot), no writes will be
-  // lost even if sync==false.
-  //
-  // In other words, a DB write with sync==false has similar
-  // crash semantics as the "write()" system call.  A DB write
-  // with sync==true has similar crash semantics to a "write()"
-  // system call followed by "fsync()".
-  //
+  // From LevelDB write options, currently we don't use this
   // Default: false
   bool sync;
 
