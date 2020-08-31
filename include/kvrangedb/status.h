3b2d10ab6ed25e137f8ae3282cf861728be04548
Mian Qin <celery1124@tamu.edu>
2020-08-31 14:54:19 -0500

    minor change

--- include/kvrangedb/status.h
+++ include/kvrangedb/status.h
@@ -85,8 +85,29 @@ class Status {
     return (state_ == NULL) ? kOk : static_cast<Code>(state_[4]);
   }
 
-  Status(Code code, const Slice& msg, const Slice& msg2);
-  static const char* CopyState(const char* s);
+  Status(Code code, const Slice& msg, const Slice& msg2) {
+    assert(code != kOk);
+    const uint32_t len1 = msg.size();
+    const uint32_t len2 = msg2.size();
+    const uint32_t size = len1 + (len2 ? (2 + len2) : 0);
+    char* result = new char[size + 5];
+    memcpy(result, &size, sizeof(size));
+    result[4] = static_cast<char>(code);
+    memcpy(result + 5, msg.data(), len1);
+    if (len2) {
+      result[5 + len1] = ':';
+      result[6 + len1] = ' ';
+      memcpy(result + 7 + len1, msg2.data(), len2);
+    }
+    state_ = result;
+  };
+  static const char* CopyState(const char* state) {
+    uint32_t size;
+    memcpy(&size, state, sizeof(size));
+    char* result = new char[size + 5];
+    memcpy(result, state, size + 5);
+    return result;
+  };
 };
 
 inline Status::Status(const Status& s) {
